6b6b1cc45a81f6d14e8b52e8d3a46e51fe194507
Klavdija <klavdija@pop-os.localdomain>
2020-01-02 15:29:04 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 import { ValidationException } from './models/exceptions/validation.exception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('v1');
   app.useGlobalPipes(
     new ValidationPipe({
